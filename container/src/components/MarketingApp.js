64f2770447e6d55bfd55d54d1ce24cefa9668371
Camilo Figueroa <cigotete@gmail.com>
2024-03-18 21:31:11 -0500

    Container project. Updating history.

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,13 +1,16 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory(); // copy browser history
 
   useEffect(() => {
     mount(ref.current, {
       onNavigate: ({ pathname: nextPathName }) => {
         console.log(nextPathName);
+        history.push(nextPathName);
       },
     });
   });
