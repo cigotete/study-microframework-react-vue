150cc895f61ff88475210dbc9317652df3e33b62
Camilo Figueroa <cigotete@gmail.com>
2024-03-18 22:10:38 -0500

    Container project. Receiving location data from Marketing to update history.

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -7,7 +7,7 @@ export default () => {
   const history = useHistory(); // copy browser history
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathName }) => {
         console.log(nextPathName);
 
@@ -17,7 +17,9 @@ export default () => {
         }
       },
     });
-  });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
